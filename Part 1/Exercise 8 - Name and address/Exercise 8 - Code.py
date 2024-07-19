ad87249dae3f7051f7da5bf7904984050147bac2
Abel Abraham <107558514+AbelAbraham77@users.noreply.github.com>
2024-07-19 09:16:08 +0530

    Delete Part 1 directory

--- Part 1/Exercise 8 - Name and address/Exercise 8 - Code.py
+++
@@ -1,8 +0,0 @@
-gname=input("Given name: ")
-fname=input("Family name: ")
-sadd=input("Street address: ")
-candpcode=input("City and postal code: ")
-
-print(gname, fname)
-print(sadd)
-print(candpcode)
