b94ae170ab010aabff12f753007890c7f2a9a6ac
Abel Abraham <107558514+AbelAbraham77@users.noreply.github.com>
2024-07-20 16:52:44 +0530

    Add files via upload

---
+++ Part 1/part01-08_name_and_address/src/name_and_address.py
@@ -0,0 +1,8 @@
+gname=input("Given name: ")
+fname=input("Family name: ")
+sadd=input("Street address: ")
+candpcode=input("City and postal code: ")
+
+print(gname, fname)
+print(sadd)
+print(candpcode)
