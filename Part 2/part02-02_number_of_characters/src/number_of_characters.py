062aa2f191ba42e1b84ed6c337db48e579481048
Abel Abraham <107558514+AbelAbraham77@users.noreply.github.com>
2024-07-20 17:21:19 +0530

    Add files via upload

---
+++ Part 2/part02-02_number_of_characters/src/number_of_characters.py
@@ -0,0 +1,8 @@
+word=input("Please type in a word: ")
+
+n=len(word)
+
+if n>1:
+    print(f"There are {n} letters in the word {word}")
+
+print("Thank you!")
---
+++ Part 2/part02-03_typecasting/src/typecasting.py
@@ -0,0 +1,6 @@
+num=float(input("Please type in a number: "))
+i=int(num)
+d=num-i
+
+print(f"Integer part: {i}")
+print(f"Decimal part: {d}")
